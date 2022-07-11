5bb95d012d193ad5a66513d31e6c022a3148d658
halilesmer <halil@esmer.de>
2022-07-11 11:16:33 +0200

    refactoring

--- src/Cards.js
+++ src/Cards.js
@@ -1,20 +1,23 @@
 import React from "react";
-import {  Row, Col, Card, CardGroup } from "react-bootstrap";
+import { Row, Col, Card, CardGroup } from "react-bootstrap";
 
-export const Cards = ({data}) => {
-    console.log("data: ", data);
+export const Cards = ({ data }) => {
+  console.log("data: ", data);
   return (
-   <> {
-      data.map((item) => { return (
-                <Col key={item.id}>
-                
-                        <Card>
-                          <Card.Img className="card-image" variant="top" src={item.image} />
-                        </Card>
-              
-        </Col>
-      );})}
-      </>
-    
+      <Row xs={1} md={2} className="g-4">
+        {data.map((item) => {
+          return (
+            <Col key={item.id}>
+              <Card>
+                <Card.Img
+                  className="card-image"
+                  variant="top"
+                  src={item.image}
+                />
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
   );
 };
