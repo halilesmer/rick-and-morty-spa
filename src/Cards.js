111c85ebfb4a5b1511e75e0430f20ae96d702e1b
halilesmer <halil@esmer.de>
2022-07-11 11:47:27 +0200

    added flip card with text

--- src/Cards.js
+++ src/Cards.js
@@ -2,7 +2,37 @@ import React from "react";
 import { Row, Col, Card, CardGroup } from "react-bootstrap";
 
 export const Cards = ({ data }) => {
-  console.log("data: ", data);
+  return (
+      <Row xs={1} md={2} className="g-4">
+        {data.map((item) => {
+          return (
+            <Col key={item.id}>
+              <Card>
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <Card.Img
+                        className="card-image"
+                        variant="top"
+                        src={item.image}
+                      />
+                    </div>
+                    <div class="flip-card-back">
+                      <h1>{item.name}</h1>
+                      <button>Learn More</button>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+  );
+};
+
+
+/* export const Cards = ({ data }) => {
   return (
       <Row xs={1} md={2} className="g-4">
         {data.map((item) => {
@@ -21,3 +51,4 @@ export const Cards = ({ data }) => {
       </Row>
   );
 };
+ */
